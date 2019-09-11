4b74372f28a90d4d7bf7e45b601230373327cb87
yves848 <yves.godart@gmail.com>
2019-09-11 20:25:29 +0200

    basic html & css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GlobalMaterialModuleModule } from './global-material-module/global-material-module.module';
 
 @NgModule({
   declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxElectronModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, NgxElectronModule, BrowserAnimationsModule, GlobalMaterialModuleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
